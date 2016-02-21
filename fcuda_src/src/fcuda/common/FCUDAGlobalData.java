4968a5c8cf23b88fd38b500d94b2ef0010db25ee
Sitao Huang <huangsitao@gmail.com>
2016-02-20 23:31:22 -0600

    update license information

--- fcuda_src/src/fcuda/common/FCUDAGlobalData.java
+++ fcuda_src/src/fcuda/common/FCUDAGlobalData.java
@@ -1,3 +1,26 @@
+//============================================================================//
+//    FCUDA
+//    Copyright (c) <2016> 
+//    <University of Illinois at Urbana-Champaign>
+//    <University of California at Los Angeles> 
+//    All rights reserved.
+// 
+//    Developed by:
+// 
+//        <ES CAD Group & IMPACT Research Group>
+//            <University of Illinois at Urbana-Champaign>
+//            <http://dchen.ece.illinois.edu/>
+//            <http://impact.crhc.illinois.edu/>
+// 
+//        <VAST Laboratory>
+//            <University of California at Los Angeles>
+//            <http://vast.cs.ucla.edu/>
+// 
+//        <Hardware Research Group>
+//            <Advanced Digital Sciences Center>
+//            <http://adsc.illinois.edu/>
+//============================================================================//
+
 package fcuda.common;
 
 import java.util.*;
